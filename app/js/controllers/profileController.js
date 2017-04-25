51ed406a9c85218a4b77086765d3077409fa72c5
Mana <sabrina.mardjoeki@gmail.com>
2017-04-25 19:57:53 +0200

    creating and updating group

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -1,4 +1,15 @@
 angular.module('app')
-    .controller('ProfileController', function($scope, CurrentUser) {
-      $scope.user = CurrentUser.user();
+    .controller('ProfileController', function($scope, CurrentUser, UserService) {
+        UserService.getOne(CurrentUser.user()._id).then(function(res) {
+            $scope.user = res.data;
+        });
+        $(document).ready(function() {
+            // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+            $('.modal').modal();
+        });
+        var id = CurrentUser.user()._id;
+        console.log(id);
+        $scope.createGroup = function() {
+            UserService.update($scope.user._id, $scope.user);
+        };
     });
