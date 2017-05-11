f6f53d6daa08773c65b0d1231ccf85c6aa5099e5
TChristal <thomaschristal@gmail.com>
2017-05-11 15:13:45 +0200

    modif input mofo

--- app/js/controllers/createEventController.js
+++ app/js/controllers/createEventController.js
@@ -1,35 +1,35 @@
 angular.module('app')
-    .controller('CreateEventController', function($scope, $state, CurrentUser, EventService, LocalService) {
-        $(document).ready(function() {
-            $('select').material_select();
-        });
-        $scope.myDate = new Date();
-        $scope.minDate = new Date(
-          $scope.myDate.getFullYear(),
-          $scope.myDate.getMonth() - 2,
-          $scope.myDate.getDate());
-          $scope.maxDate = new Date(
-            $scope.myDate.getFullYear(),
-            $scope.myDate.getMonth() + 2,
-            $scope.myDate.getDate());
-        $scope.validateDate = function() {
-            if ($scope.dateForm.$valid) {
-                var event = {
-                    name: $scope.name,
-                    liste: [],
-                    invitations: [],
-                    categorie: $scope.typeEvent,
-                    startDate: $scope.startDate,
-                    startTime: $scope.startTime,
-                    endDate: $scope.endDate,
-                    endTime: $scope.endTime,
-                    author: CurrentUser.user()._id
-                };
-                EventService.create(event).then(function(res) {
-                    LocalService.set('eventId', res.data.event._id);
-                }).then(function() {
-                    $state.go('user.invitations');
-                });
-            }
-        };
+  .controller('CreateEventController', function($scope, $state, CurrentUser, EventService, LocalService) {
+    $(document).ready(function() {
+      $('select').material_select();
     });
+    $scope.myDate = new Date();
+    $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+    $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
+    $scope.validateDate = function() {
+      if ($scope.dateForm.$valid) {
+        var event = {
+          name: $scope.name,
+          liste: [],
+          invitations: [],
+          categorie: $scope.typeEvent,
+          startDate: $scope.startDate,
+          startTime: $scope.startTime,
+          endDate: $scope.endDate,
+          endTime: $scope.endTime,
+          author: CurrentUser.user()._id
+        };
+        EventService.create(event).then(function(res) {
+          LocalService.set('eventId', res.data.event._id);
+        }).then(function() {
+          $state.go('user.invitations');
+        });
+      }
+    };
+  });
