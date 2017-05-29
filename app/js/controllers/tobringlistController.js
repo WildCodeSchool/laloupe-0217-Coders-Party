d7b480bc395060738e2e3d628b584ba2d28f6028
Mana <sabrina.mardjoeki@gmail.com>
2017-05-29 15:39:54 +0200

    message alert on the bringlist page

--- app/js/controllers/tobringlistController.js
+++ app/js/controllers/tobringlistController.js
@@ -5,30 +5,37 @@ angular.module('app')
             $scope.event = res.data;
             console.log($scope.event);
 
-        $scope.newEl = "";
-        $scope.incQty = function(index) {
-            $scope.event.elements.toBring[index].qty++;
-            $scope.event.elements.toBring[index].bringedQty++;
-            console.log($scope.event.elements);
-        };
-        $scope.decQty = function() {
-            $scope.event.elements.toBring[index].qty--;
-            $scope.event.elements.toBring[index].bringedQty--;
-        };
-        $scope.addElement = function() {
-            $scope.event.elements.toBring.push({
-                bringedQty: 0,
-                qty: 0,
-                value: $scope.newEl
-            });
-            console.log($scope.event);
             $scope.newEl = "";
-        };
-        $scope.valider = function() {
-            EventService.update(id, $scope.event).then(function() {
-                EventService.sendInvitation(id);
-                $state.go('user.happyEvent');
-            });
-        };
-      });
+            $scope.incQty = function(index) {
+                $scope.event.elements.toBring[index].qty++;
+                $scope.event.elements.toBring[index].bringedQty++;
+                console.log($scope.event.elements);
+            };
+            $scope.decQty = function() {
+                $scope.event.elements.toBring[index].qty--;
+                $scope.event.elements.toBring[index].bringedQty--;
+            };
+            $scope.addElement = function() {
+              $scope.show = false;
+                $scope.event.elements.toBring.push({
+                    bringedQty: 0,
+                    qty: 0,
+                    value: $scope.newEl
+                });
+                console.log($scope.event);
+                $scope.newEl = "";
+            };
+            $scope.show = false;
+            $scope.valider = function() {
+                if ($scope.event.elements.toBring.length > 0) {
+
+                    EventService.update(id, $scope.event).then(function() {
+                        EventService.sendInvitation(id);
+                        $state.go('user.happyEvent');
+                    });
+                } else {
+                    $scope.show = true;
+                }
+            };
+        });
     });
