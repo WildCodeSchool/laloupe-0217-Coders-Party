84e8d9a2400183780f6784c256eb4d4308a269a8
Mana <sabrina.mardjoeki@gmail.com>
2017-06-02 10:31:44 +0200

    add click on carousel-item

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,9 +1,12 @@
 angular.module('app')
-    .controller('MainController', function($scope, EventService) {
+    .controller('MainController', function($scope, EventService, $location) {
         EventService.getAll().then(function(res) {
             $scope.events = res.data;
             $(document).ready(function() {
                 $('.carousel').carousel();
             });
+            $scope.moveToEvent = function(value) {
+                $location.path('/user/event/id/' + value);
+            };
         });
     });
