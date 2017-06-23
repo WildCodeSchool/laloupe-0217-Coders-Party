b38dbeee5327777be4cf55d79cafb2235db6fcbe
TChristal <thomaschristal@gmail.com>
2017-06-23 11:34:31 +0200

    add xoauth2 config

--- server/api/mailer/config.js
+++ server/api/mailer/config.js
@@ -1,14 +1,19 @@
 import nodemailer from 'nodemailer';
 import smtpTransport from 'nodemailer-smtp-transport';
+import xoauth2 from 'xoauth2';
 
 module.exports = () => {
   return nodemailer.createTransport(smtpTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
+    service: 'gmail',
     secure: true,
     auth: {
-      user: "codersparty@gmail.com",
-      pass: "c0d3r5p4rtywcs"
+        xoauth2: xoauth2.createXOAuth2Generator({
+            user: 'codersparty@gmail.com',
+            clientId: '608155903190-91aeo4ou32rijaibkgso1dsuvf2r1ant.apps.googleusercontent.com',
+            clientSecret: 'mZZBqeHfMQsM_C9IebMs9iXS',
+            refreshToken: '1/rcxUYRhxmpIVEGvM10nM7QAL35Fe6O-zhUD6cyH6ggo',
+            accessToken: 'ya29.GltyBC44ox8Q55-_t8q3scCOQnQtjdHKnHnVLlQ-3NTSgbGYu5oaK4GVJy0jOZLThzeVSDKNn0Nt_4gMNt-bhdQgwovHjv87eXFd9dq_3i6kwSrLIecmMI3RQXdH'
+        })
     }
   }));
 };
